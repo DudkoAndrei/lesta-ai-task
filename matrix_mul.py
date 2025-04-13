06b324a2d939e3580b4ded7c20100416f4d880b9
DudkoAndrei <dudkoandrei@gmail.com>
2025-04-13 09:16:28 +0000

    Добавил скрипт для перемножения матриц

---
+++ matrix_mul.py
@@ -0,0 +1,20 @@
+import sys 
+from matrix_lib import Matrix
+
+def write_matrix(matrix, filename):
+    with open(filename, 'w') as f:
+        f.write("%d\n%d\n" % (matrix.rows, matrix.cols))
+        f.write(" ".join(map(str, matrix.data)))
+
+def read_matrix(filename):
+    with open(filename) as f:
+        rows = int(f.readline())
+        cols = int(f.readline())
+        data = map(float, f.readline().split())
+        return Matrix(rows, cols, data)
+
+if __name__ == "__main__":
+    matrix1 = read_matrix(sys.argv[1])
+    matrix2 = read_matrix(sys.argv[2])
+    result = matrix1.multiply(matrix1)
+    write_matrix(result, sys.argv[3])
